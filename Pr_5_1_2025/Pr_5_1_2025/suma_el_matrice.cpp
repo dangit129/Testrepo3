4828727d1affe223661bbde530af1f722dae59ed
dangit129 <danut_so@yahoo.com>
2025-04-30 18:47:53 +0300

    Commit la primul proiect VS

---
+++ Pr_5_1_2025/Pr_5_1_2025/suma_el_matrice.cpp
@@ -0,0 +1,50 @@
+/* Suma elementelor a
+   unei matrici a[100][100] cu m - linii si n - coloane */
+
+#include<iostream>
+using namespace std;
+#include <conio.h>;
+
+void main()
+{
+	int m,n,i,j,s,a[100][100];
+
+	cout<<"Introduceti nr linii, m = ";
+	cin>>m;
+
+	cout<<"Introduceti nr coloane, n = ";
+	cin>>n;
+
+	for (i=0; i<m; i++)
+	{
+      for (j=0; j<n; j++)
+	  {
+		  cout<<"Introduceti a["<<i<<"]"<<"["<<j<<"] = ";
+		  cin>>a[i][j];
+	  }
+	}
+
+	s=0;
+
+   for (i=0; i<m; i++)
+	{
+      for (j=0; j<n; j++)
+	  {
+	//	  if (i==j)
+	//	  {
+	//	    s=s+a[i][j];
+	//	  }
+
+		  if (i==n-j-1)
+		  {
+		    s=s+a[i][j];
+		  }
+
+
+	  }
+	}
+
+   cout<<"Suma elementelor matricei este : "<<s<<endl;
+
+   getch();
+}
